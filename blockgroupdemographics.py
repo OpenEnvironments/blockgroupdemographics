35f02aa388371fb8daf32933095e33485199db4c
Michael Bryan <41010416+michaelbbryan@users.noreply.github.com>
2022-09-01 10:56:57 -0400

    Update blockgroupdemographics.py

--- blockgroupdemographics.py
+++ blockgroupdemographics.py
@@ -4,16 +4,16 @@
 # blockgroupdemographics
 ## Overview
 ---
-This progran combines geographic and demographic data from the U.S. Census Bureau 
+This progran re-shares cartographic and demographic data from the U.S. Census Bureau 
 to provide an obvious supplement to Open Environments Block Group publications.
-These results do not reflect any proprietary or predictive model. Rather, they are
+These results do not reflect any proprietary or predictive model. Rather, they extract from
 Census Bureau results with some proportions and aggregation rules applied. For additional
 support or more detail, please see the Census Bureau citations below.
 
-Geographics refer to shapefiles shared in the Census TIGER/Line publications. Block
+Cartographics refer to shapefiles shared in the Census TIGER/Line publications. Block
 Group areas are updated annually, with major revisions accompanying the Decennial Census
 at the turn of each decade. These shapes are useful for visualizing estimates as a
-map and relating geographics based upon geo-operations like overlapping. This data is
+map and relating geographies based upon geo-operations like overlapping. This data is
 kept in a geodatabase file format and requires the geopandas package and its supporting
 fiona and DAL software.
 
@@ -21,14 +21,49 @@ Demographics are taken from popular variables in the American Community Survey (
 including age, race, income, education and family structure. This data simply requires
 csv reader software or pythons pandas package.
 
+## Files
+While the demographic data has many columns, the cartographic data has a very, very 
+large column called "geometry" storing the many-point boundaries of each shape. So,
+this process saves the data separately, with demographics columns in a csv file named 
+YYYYblcokgroupdemographics.csv. The cartographic column, 'geometry', is shared as 
+file named YYYYblockgroupdemographics-geometry.pkl. This file needs an installation 
+of geopandas, fiona and DAL software.
+
 More details on the ACS variables selected and derivation rules applied can be found in
-the commentary docstrings of the get_demogs() and combine_derive() functions below.
+the commentary docstrings of the functions below.
+
+## Demonstration
+```
+import pandas as pd
+import numpy as np
+import geopandas as gpd
+import geoplot
+
+# The Block Group Voting dataset
+bgv = pd.read_csv('D:\\Open Environments\\data\\Open Environments\\blockgroupvoting\\2019\\2019blockgroupvoting.csv',
+                 converters={'BLOCKGROUP_GEOID': '{:0>12}'.format})
+# This demographic and  derived dataset
+bgd = pd.read_csv('C:\\Users\\michael\\Documents\\2019blockgroupdemographics.csv',
+                 converters={'GEOID': '{:0>12}'.format}, index=False)
+# The cartographic shapes of each block group
+bgc = gpd.read_picle('C:\\Users\\michael\\Documents\\2019blockgroupdemographics-geometry.pkl')
+
+# What proportion of voters voted Republican in the 2020 election
+mix1 = pd.merge(bgc,bgv, left_on='GEOID', right_on="BLOCKGROUP_GEOID",  how='left')
+mix2 = pd.merge(mix1,bgd, on='GEOID', how='left')
+mix2["RepRate"] = mix2.REP / (mix2.REP + mix2.DEM)
+
+geoplot.choropleth(mix2[~mix2.STATE.isin(["HI","AK"])],
+                   hue="RepRate", cmap="Reds", 
+                   figsize=(15,7), legend=True,
+                   linewidth=0)
+```
 
 ## Citations:
 ---
   “TIGER/Line and TIGER-Related Products Electronic Resource: TIGER, Topologically Integrated Geographic Encoding and Referencing System.” Index of /Geo/Tiger/TIGER2019/BG, U.S. Census Bureau, 10 Feb. 2022, https://www2.census.gov/geo/tiger/TIGER2019/BG/. 
   “American Community Survey 5-Year Data (2009-2020).” Census.gov, US Census Bureau, 8 Mar. 2022, https://www.census.gov/data/developers/data-sets/acs-5year.html. 
-
+  Bryan, Michael, 2022, "U.S. Voting by Census Block Groups", https://doi.org/10.7910/DVN/NKNWBX, Harvard Dataverse, V3, UNF:6:OlQ9kJRJ1BDUEYRNlqusJA== [fileUNF]
 """
 
 import pandas as pd
@@ -37,6 +72,7 @@ import numpy as np
 np.seterr(divide = 'ignore') 
 import psutil
 import geopandas as gpd
+import geoplot
 import logging
 
 logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
@@ -49,7 +85,6 @@ consoleHandler.setFormatter(logFormatter)
 rootLogger.addHandler(consoleHandler)
 logging.basicConfig(level=logging.DEBUG)
 
-print("Starting:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
 
 def get_demogs(frompath):
     """
@@ -100,18 +135,19 @@ def get_demogs(frompath):
         B01001e41   Female 55 to 59 years                   B15003e24   Professional school degree              
         B01001e42   Female 60 and 61 years                  B15003e25   Doctorate degree                        
         B01001e43   Female 62 to 64 years                       
-        B01001e44   Female 65 and 66 years                      
-        B01001e45   Female 67 to 69 years                       
-        B01001e46   Female 70 to 74 years                       
-        B01001e47   Female 75 to 79 years                       
+        B01001e44   Female 65 and 66 years                  ALAND       Land area of the block group in square meters
+        B01001e45   Female 67 to 69 years                   AWATER      Water area of the block group in square meters        
+        B01001e46   Female 70 to 74 years                   INTPTLAT    Longitude of the internal point (centroid)
+        B01001e47   Female 75 to 79 years                   INTPTLON    Latitude of the internal point (centroid)
         B01001e48   Female 80 to 84 years                       
         B01001e49   Female 85 years and over                    
     """
     print("Pulling ACS demographics:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
     # initialize
-    varlist = ['X01_AGE_AND_SEX.B01003e1','X01_AGE_AND_SEX.B01001e1','X01_AGE_AND_SEX.B01001e2','X01_AGE_AND_SEX.B01001e3','X01_AGE_AND_SEX.B01001e4','X01_AGE_AND_SEX.B01001e5','X01_AGE_AND_SEX.B01001e6','X01_AGE_AND_SEX.B01001e7','X01_AGE_AND_SEX.B01001e8','X01_AGE_AND_SEX.B01001e9','X01_AGE_AND_SEX.B01001e10','X01_AGE_AND_SEX.B01001e11','X01_AGE_AND_SEX.B01001e12','X01_AGE_AND_SEX.B01001e13','X01_AGE_AND_SEX.B01001e14','X01_AGE_AND_SEX.B01001e15','X01_AGE_AND_SEX.B01001e16','X01_AGE_AND_SEX.B01001e17','X01_AGE_AND_SEX.B01001e18','X01_AGE_AND_SEX.B01001e19','X01_AGE_AND_SEX.B01001e20','X01_AGE_AND_SEX.B01001e21','X01_AGE_AND_SEX.B01001e22','X01_AGE_AND_SEX.B01001e23','X01_AGE_AND_SEX.B01001e24','X01_AGE_AND_SEX.B01001e25','X01_AGE_AND_SEX.B01001e26','X01_AGE_AND_SEX.B01001e27','X01_AGE_AND_SEX.B01001e28','X01_AGE_AND_SEX.B01001e29','X01_AGE_AND_SEX.B01001e30','X01_AGE_AND_SEX.B01001e31','X01_AGE_AND_SEX.B01001e32','X01_AGE_AND_SEX.B01001e33','X01_AGE_AND_SEX.B01001e34','X01_AGE_AND_SEX.B01001e35','X01_AGE_AND_SEX.B01001e36','X01_AGE_AND_SEX.B01001e37','X01_AGE_AND_SEX.B01001e38','X01_AGE_AND_SEX.B01001e39','X01_AGE_AND_SEX.B01001e40','X01_AGE_AND_SEX.B01001e41','X01_AGE_AND_SEX.B01001e42','X01_AGE_AND_SEX.B01001e43','X01_AGE_AND_SEX.B01001e44','X01_AGE_AND_SEX.B01001e45','X01_AGE_AND_SEX.B01001e46','X01_AGE_AND_SEX.B01001e47','X01_AGE_AND_SEX.B01001e48','X01_AGE_AND_SEX.B01001e49','X19_INCOME.B19013e1','X19_INCOME.B19001e1','X19_INCOME.B19001e2','X19_INCOME.B19001e3','X19_INCOME.B19001e4','X19_INCOME.B19001e5','X19_INCOME.B19001e6','X19_INCOME.B19001e7','X19_INCOME.B19001e8','X19_INCOME.B19001e9','X19_INCOME.B19001e10','X19_INCOME.B19001e11','X19_INCOME.B19001e12','X19_INCOME.B19001e13','X19_INCOME.B19001e14','X19_INCOME.B19001e15','X19_INCOME.B19001e16','X19_INCOME.B19001e17','X15_EDUCATIONAL_ATTAINMENT.B15003e1','X15_EDUCATIONAL_ATTAINMENT.B15003e2','X15_EDUCATIONAL_ATTAINMENT.B15003e3','X15_EDUCATIONAL_ATTAINMENT.B15003e4','X15_EDUCATIONAL_ATTAINMENT.B15003e5','X15_EDUCATIONAL_ATTAINMENT.B15003e6','X15_EDUCATIONAL_ATTAINMENT.B15003e7','X15_EDUCATIONAL_ATTAINMENT.B15003e8','X15_EDUCATIONAL_ATTAINMENT.B15003e9','X15_EDUCATIONAL_ATTAINMENT.B15003e10','X15_EDUCATIONAL_ATTAINMENT.B15003e11','X15_EDUCATIONAL_ATTAINMENT.B15003e12','X15_EDUCATIONAL_ATTAINMENT.B15003e13','X15_EDUCATIONAL_ATTAINMENT.B15003e14','X15_EDUCATIONAL_ATTAINMENT.B15003e15','X15_EDUCATIONAL_ATTAINMENT.B15003e16','X15_EDUCATIONAL_ATTAINMENT.B15003e17','X15_EDUCATIONAL_ATTAINMENT.B15003e18','X15_EDUCATIONAL_ATTAINMENT.B15003e19','X15_EDUCATIONAL_ATTAINMENT.B15003e20','X15_EDUCATIONAL_ATTAINMENT.B15003e21','X15_EDUCATIONAL_ATTAINMENT.B15003e22','X15_EDUCATIONAL_ATTAINMENT.B15003e23','X15_EDUCATIONAL_ATTAINMENT.B15003e24','X15_EDUCATIONAL_ATTAINMENT.B15003e25','X02_RACE.B02001e1','X02_RACE.B02001e2','X02_RACE.B02001e3','X02_RACE.B02001e4','X02_RACE.B02001e5','X02_RACE.B02001e6','X02_RACE.B02001e7','X02_RACE.B02001e8','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e1','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e2','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e3','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e1','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e2','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e7','X23_EMPLOYMENT_STATUS.B23025e1','X23_EMPLOYMENT_STATUS.B23025e2','X23_EMPLOYMENT_STATUS.B23025e7','X09_CHILDREN_HOUSEHOLD_RELATIONSHIP.B09019e1','X09_CHILDREN_HOUSEHOLD_RELATIONSHIP.B09018e1']
+    varlist = ['X01_AGE_AND_SEX.B01003e1','X01_AGE_AND_SEX.B01001e1','X01_AGE_AND_SEX.B01001e2','X01_AGE_AND_SEX.B01001e3','X01_AGE_AND_SEX.B01001e4','X01_AGE_AND_SEX.B01001e5','X01_AGE_AND_SEX.B01001e6','X01_AGE_AND_SEX.B01001e7','X01_AGE_AND_SEX.B01001e8','X01_AGE_AND_SEX.B01001e9','X01_AGE_AND_SEX.B01001e10','X01_AGE_AND_SEX.B01001e11','X01_AGE_AND_SEX.B01001e12','X01_AGE_AND_SEX.B01001e13','X01_AGE_AND_SEX.B01001e14','X01_AGE_AND_SEX.B01001e15','X01_AGE_AND_SEX.B01001e16','X01_AGE_AND_SEX.B01001e17','X01_AGE_AND_SEX.B01001e18','X01_AGE_AND_SEX.B01001e19','X01_AGE_AND_SEX.B01001e20','X01_AGE_AND_SEX.B01001e21','X01_AGE_AND_SEX.B01001e22','X01_AGE_AND_SEX.B01001e23','X01_AGE_AND_SEX.B01001e24','X01_AGE_AND_SEX.B01001e25','X01_AGE_AND_SEX.B01001e26','X01_AGE_AND_SEX.B01001e27','X01_AGE_AND_SEX.B01001e28','X01_AGE_AND_SEX.B01001e29','X01_AGE_AND_SEX.B01001e30','X01_AGE_AND_SEX.B01001e31','X01_AGE_AND_SEX.B01001e32','X01_AGE_AND_SEX.B01001e33','X01_AGE_AND_SEX.B01001e34','X01_AGE_AND_SEX.B01001e35','X01_AGE_AND_SEX.B01001e36','X01_AGE_AND_SEX.B01001e37','X01_AGE_AND_SEX.B01001e38','X01_AGE_AND_SEX.B01001e39','X01_AGE_AND_SEX.B01001e40','X01_AGE_AND_SEX.B01001e41','X01_AGE_AND_SEX.B01001e42','X01_AGE_AND_SEX.B01001e43','X01_AGE_AND_SEX.B01001e44','X01_AGE_AND_SEX.B01001e45','X01_AGE_AND_SEX.B01001e46','X01_AGE_AND_SEX.B01001e47','X01_AGE_AND_SEX.B01001e48','X01_AGE_AND_SEX.B01001e49','X19_INCOME.B19013e1','X19_INCOME.B19001e1','X19_INCOME.B19001e2','X19_INCOME.B19001e3','X19_INCOME.B19001e4','X19_INCOME.B19001e5','X19_INCOME.B19001e6','X19_INCOME.B19001e7','X19_INCOME.B19001e8','X19_INCOME.B19001e9','X19_INCOME.B19001e10','X19_INCOME.B19001e11','X19_INCOME.B19001e12','X19_INCOME.B19001e13','X19_INCOME.B19001e14','X19_INCOME.B19001e15','X19_INCOME.B19001e16','X19_INCOME.B19001e17','X15_EDUCATIONAL_ATTAINMENT.B15003e1','X15_EDUCATIONAL_ATTAINMENT.B15003e2','X15_EDUCATIONAL_ATTAINMENT.B15003e3','X15_EDUCATIONAL_ATTAINMENT.B15003e4','X15_EDUCATIONAL_ATTAINMENT.B15003e5','X15_EDUCATIONAL_ATTAINMENT.B15003e6','X15_EDUCATIONAL_ATTAINMENT.B15003e7','X15_EDUCATIONAL_ATTAINMENT.B15003e8','X15_EDUCATIONAL_ATTAINMENT.B15003e9','X15_EDUCATIONAL_ATTAINMENT.B15003e10','X15_EDUCATIONAL_ATTAINMENT.B15003e11','X15_EDUCATIONAL_ATTAINMENT.B15003e12','X15_EDUCATIONAL_ATTAINMENT.B15003e13','X15_EDUCATIONAL_ATTAINMENT.B15003e14','X15_EDUCATIONAL_ATTAINMENT.B15003e15','X15_EDUCATIONAL_ATTAINMENT.B15003e16','X15_EDUCATIONAL_ATTAINMENT.B15003e17','X15_EDUCATIONAL_ATTAINMENT.B15003e18','X15_EDUCATIONAL_ATTAINMENT.B15003e19','X15_EDUCATIONAL_ATTAINMENT.B15003e20','X15_EDUCATIONAL_ATTAINMENT.B15003e21','X15_EDUCATIONAL_ATTAINMENT.B15003e22','X15_EDUCATIONAL_ATTAINMENT.B15003e23','X15_EDUCATIONAL_ATTAINMENT.B15003e24','X15_EDUCATIONAL_ATTAINMENT.B15003e25','X02_RACE.B02001e1','X02_RACE.B02001e2','X02_RACE.B02001e3','X02_RACE.B02001e4','X02_RACE.B02001e5','X02_RACE.B02001e6','X02_RACE.B02001e7','X02_RACE.B02001e8','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e1','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e2','X03_HISPANIC_OR_LATINO_ORIGIN.B03003e3','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e1','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e2','X11_HOUSEHOLD_FAMILY_SUBFAMILIES.B11001e7','X23_EMPLOYMENT_STATUS.B23025e1','X23_EMPLOYMENT_STATUS.B23025e2','X23_EMPLOYMENT_STATUS.B23025e7','X09_CHILDREN_HOUSEHOLD_RELATIONSHIP.B09019e1','X09_CHILDREN_HOUSEHOLD_RELATIONSHIP.B09018e1',
+              'ACS_2019_5YR_BG.ALAND', 'ACS_2019_5YR_BG.AWATER', 'ACS_2019_5YR_BG.INTPTLAT', 'ACS_2019_5YR_BG.INTPTLON']
     acs = gpd.read_file(frompath,
-                        layer = frompath[frompath.rfind("/")+1:frompath.rfind(".")])[["GEOID","geometry"]] 
+          layer = frompath[frompath.rfind("/")+1:frompath.rfind(".")])[["GEOID","geometry"]] 
     # Open each layer, then collect the variables in it
     for l in set([v[:v.index(".")] for v in varlist]):  # Unique layers in the var list
         print("    Opening layer",l)
@@ -122,36 +158,13 @@ def get_demogs(frompath):
     acs["GEOID"] = acs.GEOID.str[-12:]  # original formats GEOID like 15000US721537506022
     return acs
 
-def get_shapes(fromprefix):
-    """
-    This function reads the shapes of Block Group geographies as well as their land area
-    from the Cenus' TIGER/Line publication.  The source organizes shapes state by state
-    so a reference list of states, with their FIPS codes, is read from the Open Environments
-    core repository.
-    
-    :param  fromprefix:  String containing the path where the files have been downloaded
-                         from https://www2.census.gov/geo/tiger/TIGER2019/BG/ eg
-                         
-    :return tiger: dataframe with all states and their block groups appended.
-    """
-    print("Pulling TIGER/Line geographics:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
-    states = pd.read_csv("https://raw.githubusercontent.com/OpenEnvironments/core/main/states.csv")
-    tiger = gpd.read_file(fromprefix+"01_bg").truncate(after=-1)
-    tiger.truncate()
-    for i,state in states[~states.CensusName.isna()].iterrows():
-        tiger = pd.concat([tiger, \
-                          gpd.read_file(fromprefix + "{:02d}".format(state["StateFIPS"]) + "_bg")],
-                          axis=0)
-    print("   Done:","Block Groups:",tiger.shape[0],"Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
-    return tiger
-
-def combine_derive(acs,tiger):
+def derive_elements(df):
     """
     This function accepts dataframes with ACS and TIGER Line shapes, combines them
     on Block Group geoids, then adds derivations.
     
     :param  acs, tiger: dataframes, keyed on 12 digit block group GEOID
-    :return combined: dataframe with the inputs merged columnwise with these
+    :return df: dataframe with the inputs merged columnwise with these
             derivations applied:
             
         Over65Rate        proportion of population with age over 65
@@ -167,37 +180,34 @@ def combine_derive(acs,tiger):
         logLand           natural log of the land area (sq meters) of the Block Group
     """
     print("Combining and calculating derivations:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
-    combined = pd.merge(acs,tiger,on="GEOID",how="outer")
-    print("Merge done",combined.shape)
-    combined['Over65Rate'] = (combined['B01001e20'] + combined['B01001e21'] + combined['B01001e22'] + combined['B01001e23'] + combined['B01001e24'] + combined['B01001e25'] + combined['B01001e44'] + combined['B01001e45'] + combined['B01001e46'] + combined['B01001e47'] + combined['B01001e48'] + combined['B01001e49']) / (combined['B01001e2'] + combined['B01001e26'])
-    combined['MinorityRate'] = 1 - (combined['B02001e2'] / combined['B02001e1'])
-    combined['PopDensity'] = combined['B01001e1'] / combined['ALAND']
-    combined['CollegePlusRate'] = (combined['B15003e21'] + combined['B15003e22'] + combined['B15003e23'] + combined['B15003e24'] + combined['B15003e25']) / combined['B15003e1']
-    combined['EmploymentRate'] = combined['B23025e2'] / combined['B23025e1']
-    combined['FamilyRate'] = combined['B11001e2'] / combined['B11001e1']
-    combined['ChildrenRate'] = combined['B09018e1'] / combined['B09019e1']
-    combined['Under25KRate'] = (combined['B19001e2'] + combined['B19001e3'] + combined['B19001e4'] + combined['B19001e5']) / combined['B19001e1']
-    combined['logPop'] = np.log(combined.B01001e1)
-    combined['logPopDensity'] = np.log(combined.PopDensity)
-    combined['logLand'] = np.log(combined.ALAND)
-    return combined
+    df['Over65Rate'] = (df['B01001e20'] + df['B01001e21'] + df['B01001e22'] + df['B01001e23'] + df['B01001e24'] + df['B01001e25'] + df['B01001e44'] + df['B01001e45'] + df['B01001e46'] + df['B01001e47'] + df['B01001e48'] + df['B01001e49']) / (df['B01001e2'] + df['B01001e26'])
+    df['MinorityRate'] = 1 - (df['B02001e2'] / df['B02001e1'])
+    df['PopDensity'] = df['B01001e1'] / (df['ALAND'] + df['AWATER'])
+    df['CollegePlusRate'] = (df['B15003e21'] + df['B15003e22'] + df['B15003e23'] + df['B15003e24'] + df['B15003e25']) / df['B15003e1']
+    df['EmploymentRate'] = df['B23025e2'] / df['B23025e1']
+    df['FamilyRate'] = df['B11001e2'] / df['B11001e1']
+    df['ChildrenRate'] = df['B09018e1'] / df['B09019e1']
+    df['Under25KRate'] = (df['B19001e2'] + df['B19001e3'] + df['B19001e4'] + df['B19001e5']) / df['B19001e1']
+    df['logPop'] = np.log(df.B01001e1)
+    df['logPopDensity'] = np.log(df.PopDensity)
+    df['logLand'] = np.log(df.ALAND)
+    return df
 
-def put_results(combined, fn):
+def put_results(df, fn):
     """
-    This function accepts a dataframe and filename prefix
+    This function accepts a geodataframe and filename prefix
     to generate a zipped CSV file (.zip) pandas pickle file (.pkl)
     with the filename prefix.
     """
-    combined.to_csv(fn+'.zip',
-              index=False,
-              header=True,
-              compression="zip",
-              encoding='utf-8-sig')
-    combined.to_pickle(fn+'.pkl')
+    print("Writing to files:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
+    df.drop("geometry",axis=1).to_csv(fn+'.csv', index=False)
+    df[["GEOID","geometry"]].to_pickle(fn+"-geometry.pkl") 
     return
 
 if __name__ == "__main__":
+    # This run refers to 2019 and my local file paths; replace as appropriate 
+    print("Starting:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
     acs = get_demogs("D:/Open Environments/data/census/acs/acs5/ACS_2019_5YR_BG.gdb")
-    tiger = get_shapes("D:/Open Environments/data/census/tiger/2019 blockgroups/tl_2019_")
-    combined = combine_derive(acs,tiger)
-    put_results(combined,'blockgroupdemographics')
+    complete = derive_elements(acs)
+    put_results(complete,'2019blockgroupdemographics')
+    print("Done:","Mem: {:.2f}G".format(psutil.virtual_memory()[1]/1000000000))
